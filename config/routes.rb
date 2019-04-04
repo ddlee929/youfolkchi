0e02ede1270195f9ee1b0ea8655acc6efea97457
Daniel Lee <ddlee929@gmail.com>
2019-04-04 11:17:06 -0500

    fixed header and footer for music and home pages a bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :musics
   resources :posts
   get 'pages/home'
 
---
+++ db/migrate/20190401234621_create_musics.rb
@@ -0,0 +1,10 @@
+class CreateMusics < ActiveRecord::Migration[5.1]
+  def change
+    create_table :musics do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
