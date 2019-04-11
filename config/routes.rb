800b515b8c512dd04a4d072e06b08ad32f2024fa
Daniel Lee <ddlee929@gmail.com>
2019-04-11 13:10:24 -0500

    switched from vertical albums list to three rows for the related work section

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
+  get 'pages/performance'
+
   resources :shows
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
