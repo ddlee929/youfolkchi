9805d152eda6ae5c005fd160a69d3ecf1c9a4324
Daniel Lee <ddlee929@gmail.com>
2019-08-06 20:24:31 -0700

    precompile explicitly css files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( albums.css application.css contacts.css denieds.css gigs.css admins.css )
